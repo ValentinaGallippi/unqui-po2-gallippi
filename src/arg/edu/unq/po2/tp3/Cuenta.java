55b063777d87d0389dc6a7789a51d195c8ceb27e
Valentina Gallippi <valentina.gallippi@gmail.com>
2024-04-03 13:04:34 -0300

    version inicial

--- src/arg/edu/unq/po2/tp3/Cuenta.java
+++ src/arg/edu/unq/po2/tp3/Cuenta.java
@@ -1,5 +1,24 @@
 package arg.edu.unq.po2.tp3;
 
 public class Cuenta {
-
+	private int x;
+	private int y;
+	public Cuenta(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
